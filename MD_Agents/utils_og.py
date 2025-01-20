9385cfc5f1d8b17185db41a08124822ae0fb572e
Xin Li <xenosxiny@icloud.com>
2025-01-20 14:00:53 -0700

    --modified  api code changes

--- MD_Agents/utils_og.py
+++ MD_Agents/utils_og.py
@@ -22,7 +22,7 @@ class Agent:
             self.model = genai.GenerativeModel('gemini-pro')
             self._chat = self.model.start_chat(history=[])
         elif self.model_info in ['gpt-3.5', 'gpt-4', 'gpt-4o', 'gpt-4o-mini']:
-            self.client = OpenAI(api_key='')
+            self.client = OpenAI(api_key=os.environ['openai_api_key'])
             self.messages = [
                 {"role": "system", "content": instruction},
             ]
